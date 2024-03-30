b54a98998195bb94b95d7ee8403afe208994e2e2
Nathan Avaaga <nathanavaaga@gmail.com>
2024-03-30 17:29:40 +0300

    wow

---
+++ stacks.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - prints the top of the list
+ * @head: head of the stack
+ * @counter: line number
+ * Return: no return
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
